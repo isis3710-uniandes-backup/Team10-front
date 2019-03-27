eb0bda7485712556a78e5eb692d192a99f0010aa
fox <fox@fox-VirtualBox>
2019-03-27 16:51:13 -0500

    Color Change

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -31,24 +31,24 @@ export default class Navigation extends React.Component {
 	render() {
 		return (
 			<div>
-				<Navbar color="dark" light expand="md">
+				<Navbar color="danger" expand="md">
 		          <NavbarToggler onClick={this.toggle} />
 		          <Collapse isOpen={this.state.isOpen} navbar>
 		            <Nav className="mr-auto" navbar>
 		              <NavItem>
-		                <Link to={'/categories'} className="nav-link">Categories</Link>
+		                <Link to={'/categories'} className="nav-link text-white">Categories</Link>
 		              </NavItem>
 		              <NavItem>
-		                <Link to={'/'} className="nav-link">Restaurants</Link>
+		                <Link to={'/'} className="nav-link text-white">Restaurants</Link>
 		              </NavItem>
 		              <NavItem>
-		                <Link to={'/promotions'} className="nav-link">Promotions</Link>
+		                <Link to={'/promotions'} className="nav-link text-white">Promotions</Link>
 		              </NavItem>
 		              <NavItem>
-		                <Link to={'/new'} className="nav-link">New restaurants</Link>
+		                <Link to={'/new'} className="nav-link text-white">New restaurants</Link>
 		              </NavItem>
 		              <NavItem>
-		                <Link to={'/you'} className="nav-link">Account</Link>
+		                <Link to={'/you'} className="nav-link text-white">Account</Link>
 		              </NavItem>
 		            </Nav>
 		          </Collapse>
