49c3fbf076963085be1391006151dde0a4bd148c
fox <fox@fox-VirtualBox>
2019-03-28 15:12:50 -0500

    Categories Dropdown

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -1,59 +1,74 @@
 import React from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+Collapse,
+        Navbar,
+        NavbarToggler,
+        NavbarBrand,
+        Nav,
+        NavItem,
+        NavLink,
+        UncontrolledDropdown,
+        Dropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem } from 'reactstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export default class Navigation extends React.Component {
-	constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+        this.toggle = this.toggle.bind(this);
+        this.toggle_menu = this.toggle_menu.bind(this);
+        this.state = {
+            isOpen: false,
+            dropdownOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 
-	render() {
-		return (
-			<div>
-				<Navbar color="danger" expand="md">
-		          <NavbarToggler onClick={this.toggle} />
-		          <Collapse isOpen={this.state.isOpen} navbar>
-		            <Nav className="mr-auto" navbar>
-		              <NavItem>
-		                <Link to={'/categories'} className="nav-link text-white">Categories</Link>
-		              </NavItem>
-		              <NavItem>
-		                <Link to={'/'} className="nav-link text-white">Restaurants</Link>
-		              </NavItem>
-		              <NavItem>
-		                <Link to={'/promotions'} className="nav-link text-white">Promotions</Link>
-		              </NavItem>
-		              <NavItem>
-		                <Link to={'/new'} className="nav-link text-white">New restaurants</Link>
-		              </NavItem>
-		              <NavItem>
-		                <Link to={'/you'} className="nav-link text-white">Account</Link>
-		              </NavItem>
-		            </Nav>
-		          </Collapse>
-		        </Navbar>
-			</div>
-		);
-	}
+    toggle_menu() {
+        this.setState(prevState => ({
+                dropdownOpen: !prevState.dropdownOpen
+            }));
+    }
+    render() {
+        return (
+                <div>
+                    <Navbar color="danger" expand="md">
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="mr-auto" navbar>
+                                <NavItem>
+                                    <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle_menu}>
+                                        <DropdownToggle caret>
+                                            Dropdown
+                                        </DropdownToggle>
+                                        <DropdownMenu>
+                                            <DropdownItem>Some Action</DropdownItem>
+                                        </DropdownMenu>
+                                    </Dropdown>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to={'/'} className="nav-link text-white">Restaurants</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to={'/promotions'} className="nav-link text-white">Promotions</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to={'/new'} className="nav-link text-white">New restaurants</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to={'/you'} className="nav-link text-white">Account</Link>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+                </div>
+                );
+    }
 }
