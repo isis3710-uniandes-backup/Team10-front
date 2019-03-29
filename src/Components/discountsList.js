ff0024e1c126c311ecae59336513aa57155f10ab
unknown <af.leon@uniandes.edu.co>
2019-03-29 03:22:33 -0500

    Mejoras en Discounts

--- src/Components/discountsList.js
+++ src/Components/discountsList.js
@@ -10,7 +10,7 @@ constructor(props) {
 		return (
 			<div>
 			  <Card>
-                <CardImg top width="100%" src="https://s3.ap-south-1.amazonaws.com/zzd-assets/merchant/medium/1533712638aapp2.PNG" alt="Card image cap" />
+                <CardImg top width="100%" src={this.props.discounts.image} alt="Card image cap" />
                 <CardBody>
                   <CardTitle>{this.props.discounts.dish}</CardTitle>
                   <CardSubtitle>{this.props.discounts.restaurant}</CardSubtitle>
