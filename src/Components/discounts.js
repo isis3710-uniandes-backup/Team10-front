5e5b9eec016bb30b9e5dd72c3a94b57eb75e99bc
fox <fox@fox-VirtualBox>
2019-05-19 15:09:33 -0500

    discounts graph

--- src/Components/discounts.js
+++ src/Components/discounts.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { LineChart } from 'react-charts-d3';
 import DiscountsList from './discountsList'
 const API = "http://localhost:3001/promotions"
 
 export default class Discounts extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {"discounts":[]};
+		this.state = {"discounts":[],
+                "data":[]};
 	}
 	getDiscounts(){
 		fetch(API)
@@ -16,8 +18,19 @@ export default class Discounts extends React.Component {
 	componentDidMount(){
 		this.getDiscounts()
 	}
-	
+	getLoading(){
+		return (<div className="loadingContainer"><div className="loading"></div></div>)
+	}
 	render() {
+                var graph_data = [];
+                for(var i = 0; i < this.state.discounts.length; ++i){
+                    graph_data.push({ x: this.state.discounts[i]["dish"], y: this.state.discounts[i]["price"] });
+                }
+                var data = this.state.data;
+                if(graph_data.length!=0)
+                {
+                 data = [{key: 'Prices', values:graph_data}];   
+                }
 		let promotions = this.state.discounts.map(discounts =>{
 			return(
 				<Col sm = "2">
@@ -30,6 +43,11 @@ export default class Discounts extends React.Component {
 			  <Row>
 			    {promotions}
 			  </Row>
+                                  <Row>
+                                  <Col sm = "12">
+                    {data==0?this.getLoading():<LineChart data={data} axisConfig={{ showXAxis: true, showXAxisLabel: true, xLabel: 'Dishes names', xLabelPosition: 'right', showYAxis: true, showYAxisLabel: false, yLabel: ''}}/>}
+                    </Col>
+                    </Row>
 			</Container>
 		);
 	}
